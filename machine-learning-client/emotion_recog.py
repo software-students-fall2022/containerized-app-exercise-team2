e2a41491f6ccb83ba6c96574a3bc672d24ffd220
TheAlexanderChen <alexchen7889@gmail.com>
2022-11-28 13:53:03 -0500

    Expanded on the ml to detect faces

--- machine-learning-client/emotion_recog.py
+++ machine-learning-client/emotion_recog.py
@@ -4,30 +4,44 @@ import cv2
 from torchvision import datasets, models, transforms
 from PIL import Image
 
-class model:
+#Class for the machine learning model
+class Model:
+    #Creates the model and loads in the weights for the emotion recognition model I trained
     def __init__(self):
-        model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', weights = None)
-        num_ftrs = model.fc.in_features
-        model.fc = nn.Linear(num_ftrs, 7)
-        model.load_state_dict(torch.load("emotion_recog_1.pth"))
-        model.eval()
+        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', weights = None)
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, 7)
+        self.model.load_state_dict(torch.load("machine-learning-client/emotion_recog_1.pth", map_location=torch.device('cpu')))
+        self.model.eval()
 
-    def read_picture(self, pic = None):
-        #Convert to tensor and standardize pic
-        if not pic:
-            pic = self.take_picture()
-        cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
-        pil_pic = Image.fromarray(pic)
+    #Takes a picture then transforms it for the model
+    #The model then processes it and returns 
+    def read_picture(self):
+        capture = cv2.VideoCapture(0)
+        __, pic = capture.read()
+        cascade = cv2.CascadeClassifier('machine-learning-client/haarcascade_frontalface_default.xml')
+        cv2.imshow('window',pic)
+        cv2.waitKey(0) 
+        cv2.destroyAllWindows() 
+        gray = cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
+        faces = cascade.detectMultiScale(gray, 1.1, 0)
+        for (x, y, w, h) in faces:
+            face = gray[y:y+h, x:x+w]
+        capture.release()
+        pil_pic = Image.fromarray(face)
 
         data_transforms = transforms.Compose([
             transforms.Resize(256),
-            pic.transforms.CenterCrop(224),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
-        image = data_transforms(pil_pic)
-        return model(image)
-    
-    def take_picture():
-        return VideoCapture(0).read()
 
+        image = data_transforms(pil_pic)
+        outputs = self.model(image.unsqueeze(0))
+        _, preds = torch.max(outputs, 1)
+        return preds
+
+temp = Model()
+pic = temp.read_picture()
+print(pic[0])
