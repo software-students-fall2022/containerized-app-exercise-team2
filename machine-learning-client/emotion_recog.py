697078d92d81c7ac3faa0e8f24f66443d0e96ed3
jkolib <kolibachukjohn@gmail.com>
2022-12-03 16:34:36 -0500

    modularized and added app component

--- machine-learning-client/emotion_recog.py
+++ machine-learning-client/emotion_recog.py
@@ -3,6 +3,25 @@ import torch.nn as nn
 import cv2 
 from torchvision import datasets, models, transforms
 from PIL import Image
+import pymongo
+import certifi
+import sys
+import datetime
+
+url = "mongodb+srv://admin:admin123@cluster0.b6toxnx.mongodb.net/?retryWrites=true&w=majority"
+database=None
+client=None
+ca = certifi.where()
+
+connection= pymongo.MongoClient(url, tlsCAFile=ca)
+try:
+    connection.admin.command('ping')
+    client=connection
+    database = connection["project_4"]
+    print(' *', 'Connected to MongoDB!', file=sys.stderr)
+except Exception as e:
+    print(' *', "Failed to connect to MongoDB at", file=sys.stderr)
+    print('Database connection error: ' + e, file=sys.stderr)
 
 #Class for the machine learning model
 class Model:
@@ -16,26 +35,27 @@ class Model:
         self.model.eval()
         self.emotions = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
 
-    #Takes a picture then transforms it for the model
-    #The model then processes it and returns 
-    def read_picture(self, pic = None):
-        capture = None
-        if pic is None:
-            capture = cv2.VideoCapture(0)
-            __, pic = capture.read()
+    def cap_picture(self):
+        capture = cv2.VideoCapture(0)
+        __, pic = capture.read()
+        # cv2.imread(pic)
+        # cv2.imshow('capture', pic)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        capture.release()
+        return pic
+
+    def classify(self, pic):
         cascade = cv2.CascadeClassifier('C:/Users/John Kolibachuk/Desktop/NYU/Fall 2022/software_engineering/containerized-app-exercise-team2-1/machine-learning-client/haarcascade_frontalface_default.xml')
-        # cv2.imshow('window',pic)
-        # cv2.waitKey(0) 
-        # cv2.destroyAllWindows() 
         gray = cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
         faces = cascade.detectMultiScale(gray, 1.1, 0)
         face = None
         for (x, y, w, h) in faces:
             face = gray[y:y+h, x:x+w]
-        if face is None:
-            raise Exception('No faces detected') 
-        if capture is not None:
-            capture.release()
+
+        return face
+
+    def transform(self, face):
         pil_pic = Image.fromarray(face)
 
         data_transforms = transforms.Compose([
@@ -50,10 +70,53 @@ class Model:
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
         im_rep = normalize(im_rep)
-        outputs = self.model(im_rep.unsqueeze(0))
+
+        return im_rep.unsqueeze(0)
+
+    #Takes a picture then transforms it for the model
+    #The model then processes it and returns 
+    def read_picture(self, pic = None):
+        if pic is None:
+            pic = self.cap_picture()
+        
+        face = self.classify(pic)
+
+        if face is None:
+            raise Exception('No faces detected') 
+        
+        im_rep = self.transform(face)
+        outputs = self.model(im_rep)
         _, preds = torch.max(outputs, 1)
         return self.emotions[int(preds)]
 
+def main():
+    print('This app will make use of your camera to take a photo and analyze your mood.')
+    print('Please head to URL_HERE to view a history of your moods.')
+    model = Model()
+
+    user = None
+    while(True):
+        username = input('Please enter your usename: ')
+        user = database.user.find_one({'username': username})
+
+        if user != None:
+            break
+        else:
+            print('Username cannot be found!')
+
+    # db.collection_name.find_one({'username': user, 'password': passwd})
+    # model.cap_picture()
+    print('Working...')
+    mood = model.read_picture()
+    database.mood.insert_one({
+        'mood': mood,
+        'time': datetime.datetime.now(),
+        'user': user['_id']
+    })
+    print('Done!')
+
+if __name__ == '__main__':
+    main()
 # temp = Model()
 # pic = temp.read_picture()
 # print(pic[0])
